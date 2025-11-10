6466ff07522ce3b4938dc8d91595a983a22bc835
andre zhuko <andrey_zhu_kov@mail.ru>
2025-11-10 14:35:07 +0500

    fixes

--- test/test_classes_by_store.py
+++ test/test_classes_by_store.py
@@ -1,6 +1,5 @@
-import pytest
-
 from src.class_by_category import Category
+
 
 def test_Product(Product_object):
     product = Product_object
@@ -9,9 +8,15 @@ def test_Product(Product_object):
     assert product.price == 180000.0
     assert product.quantity == 5
 
+
 def test_Category(Product_object):
     product1 = Product_object
-    category1 = Category("Смартфоны","Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",[product1])
+    category1 = Category(
+        "Смартфоны",
+        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+        [product1],
+    )
     assert category1.name == "Смартфоны"
-    assert category1.description == "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни"
+    assert (category1.description == "Смартфоны, как средство не только коммуникации, "
+                                     "но и получения дополнительных функций для удобства жизни")
     assert category1.products == [product1]
